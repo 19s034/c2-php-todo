d47b12cd244fc5f944a3bf6b040cd19b3886b1b7
2019 DSP4101 <2019DSP4101@ntsv6101.com>
2020-11-25 10:11:18 +0900

    11/25

--- resources/views/todo/edit.blade.php
+++ resources/views/todo/edit.blade.php
@@ -1,15 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="col-md-10">
+<div class="col-md-10">
             <h2 class="text-muted py-3">Todo 編集</h2>
             <form action="/todo/{{ $todo->id }}" method="POST">
+                @method('PUT')
                 @include('parts.todo.input', ['todo' => $todo])
                 <button class="btn btn-primary my-2" type="submit">更新</button>
             </form>
-            <hr>
-            @include('parts.button.back')
-        </div>
-    </div>
 @endsection
